051e39ed801b92f58190f70ddc202cd3fccb2d78
vir-th-u23 <alberto.gruning.zen@gmail.com>
2023-10-01 05:02:46 -0300

    feat: make function to print every vextor of the matrix

--- src/diagonalDifference.clj
+++ src/diagonalDifference.clj
@@ -1,15 +1,14 @@
 (ns diagonalDifference)
 
-(def arr [[11 2 4] [4 5 6] [10 8 -12]])
+(def arr [[11 2 4] 
+          [4 5 6] 
+          [10 8 -12]])
 
 arr
-
-
-
-(defn repeat-n [n, arr]
-  (dotimes [item  n]
+(defn repeat-n [numtimes arr outer-item]
+  (dotimes [item numtimes]
     (cond 
-      (= 2 item) 
+      (= outer-item item) 
       (println 
        (str 
         item 
@@ -17,7 +16,15 @@ arr
         (nth arr item
              ))))))
 
-(repeat-n 3 [10 20 30])
+(repeat-n 3 [10 20 30] 1)
+
+(defn outer-mat [numtimes arra one]
+  (dotimes [item numtimes]
+    (repeat-n numtimes arra item)
+    )
+  )
+
+(outer-mat 3 arr 1)
 
 (defn sh-current [n arr]
   (dotimes [i n]
