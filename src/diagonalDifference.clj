c875e2aa221fe06a52eb8c3f3fc6219903e610eb
vir-th-u23 <alberto.gruning.zen@gmail.com>
2023-10-01 06:19:14 -0300

    feat:remove the messages to get the values

--- src/diagonalDifference.clj
+++ src/diagonalDifference.clj
@@ -7,17 +7,12 @@
 arr
 (defn repeat-n [numtimes arr outer-item]
   (dotimes [inner-item numtimes] 
-    (def inner-vector (nth arr outer-item))
+    (let [ inner-vector (nth arr outer-item)] 
     (cond (= outer-item inner-item) 
-          (println 
-           (str  
-            " INNER T (" outer-item " " inner-item ")"  
-            (nth inner-vector inner-item))) 
-          :else 
-          (println 
-           " ";(str "INNER F" outer-item " - " inner-item)
-           )
+          (nth inner-vector inner-item)
+          :else 0
           )
+      inner-vector)
 ))
 
 (repeat-n 3 [10 [10 50 30] 30] 1)
