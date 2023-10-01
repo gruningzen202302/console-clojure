68ab7b5aace8deb06040bc2057aa4161fec2db33
vir-th-u23 <alberto.gruning.zen@gmail.com>
2023-10-01 02:12:25 -0300

    update 2 files and create 2 files to setup project repl

--- src/simpleArraySum.clj
+++ src/simpleArraySum.clj
@@ -1,4 +1,4 @@
-
+(ns simpleArraySum)
 
 ;
 ; Complete the 'simpleArraySum' function below.
@@ -18,12 +18,3 @@
           (recur (rest nums) new-sum))
       sum))))
 
-(def fptr (get (System/getenv) "OUTPUT_PATH"))
-
-(def ar-count (Integer/parseInt (clojure.string/trim (read-line))))
-
-(def ar (vec (map #(Integer/parseInt %) (clojure.string/split (clojure.string/trimr (read-line)) #" "))))
-
-(def result (simpleArraySum ar))
-
-(spit fptr (str result "\n") :append true)
