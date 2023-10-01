e7901f04f59e1c718068939871f41fb7b5745cee
vir-th-u23 <alberto.gruning.zen@gmail.com>
2023-10-01 02:59:29 -0300

    feat: add snippets for loops

--- src/simpleArraySum.clj
+++ src/simpleArraySum.clj
@@ -10,11 +10,44 @@
 
 (defn simpleArraySum [ar] (reduce + ar))
 
-(defn simpleArraySumNotReduced [ar]
-  (loop [nums ar, sum 0]
-    (if-let [n (first nums)]
-      (do
-        (let [new-sum (+ sum n)]
-          (recur (rest nums) new-sum))
-      sum))))
+(simpleArraySum [1 2 3])
+ 
+
+; BEGIN: 5j1v3z5j9v8d
+(defn repeat-n [n, arr]
+  (dotimes [item  n]
+    (println item)))
+
+(defn sh-current [n arr] 
+  (dotimes [i n] 
+    (println (nth arr i))))
+
+(sh-current 3 '(3 6 1))
+
+(defn tot-arr [num,arr]
+  (let [n num]
+    (loop [i 0 total 0]
+      (if (< i n)
+        (recur (inc i) (+ total (nth arr i)))
+        total))))
+
+
+  (loop [x 0]
+    (when (< x 10)
+      (println x)
+      (recur (+ x 2))))
+ 
+
+(repeat-n 3 '(3 6 1))
+; END: 5j1v3z5j9v8d
+
+;(defn the-sum [num, arr]
+;  (repeat-n num)))
+
+
+
+
+
+
+
 
