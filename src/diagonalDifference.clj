4112fee1afe893b4b3c354ace4af95fb3382dc9b
vir-th-u23 <alberto.gruning.zen@gmail.com>
2023-10-01 07:19:01 -0300

    feat:apply abs to get the diag difference

--- src/diagonalDifference.clj
+++ src/diagonalDifference.clj
@@ -19,34 +19,11 @@ arr
 
 (apply + (vertical 3 arr)) 
 
-;; (def my-vec [2 4 4])
-;; (def call-g-sum (get-sum 3 my-vec 1))
-;; call-g-sum
+(defn diagonal-difference [num outer-arr]
+  (let [left-to-right (apply + (vertical num outer-arr))
+        right-to-left (apply + (vertical num (reverse outer-arr)))]
+    (- left-to-right right-to-left)
+    )
+  )
 
-
-;; (defn repeat-n [numtimes arr outer-item]
-;;    (dotimes [inner-item numtimes] 
-;;      (let [ inner-vector (nth arr outer-item)] 
-;;        (cond (= outer-item inner-item) 
-;;           (nth inner-vector inner-item) :else 0) 
-;;        inner-vector)
-;;      )
-;; )
-
-;; (repeat-n 3 [10 [10 50 30] 30] 1)
-
-;; (defn outer-mat [numtimes arra ]
-;;   (dotimes [item numtimes]
-;;     (repeat-n numtimes arra item)
-;;     )
-;;   )
-
-;; (outer-mat 3 arr )
-
-;; (defn sh-current [n arr]
-;;   (dotimes [i n]
-;;     (println (nth arr i))))
-
-
-;; (sh-current 3 arr)
-
+(Math/abs(diagonal-difference 3 arr))
