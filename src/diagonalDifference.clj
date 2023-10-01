8f36d71e503ead976a7ad012fbb15582a2f878f9
vir-th-u23 <alberto.gruning.zen@gmail.com>
2023-10-01 07:13:25 -0300

    feat: reduce the array to get the sum

--- src/diagonalDifference.clj
+++ src/diagonalDifference.clj
@@ -5,30 +5,48 @@
           [10 8 -12]])
 
 arr
-(defn repeat-n [numtimes arr outer-item]
-  (dotimes [inner-item numtimes] 
-    (let [ inner-vector (nth arr outer-item)] 
-    (cond (= outer-item inner-item) 
-          (nth inner-vector inner-item)
-          :else 0
-          )
-      inner-vector)
-))
 
-(repeat-n 3 [10 [10 50 30] 30] 1)
+(defn get-sum [numtimes the-vector outer-item]
+    (apply + the-vector))
 
-(defn outer-mat [numtimes arra ]
-  (dotimes [item numtimes]
-    (repeat-n numtimes arra item)
-    )
+(defn vertical [num outer-arr] 
+    (vec (for [item (range num)] 
+           (nth (nth outer-arr item) item)
+           )
+         )
+    
   )
 
-(outer-mat 3 arr )
+(apply + (vertical 3 arr)) 
 
-(defn sh-current [n arr]
-  (dotimes [i n]
-    (println (nth arr i))))
+;; (def my-vec [2 4 4])
+;; (def call-g-sum (get-sum 3 my-vec 1))
+;; call-g-sum
 
 
-(sh-current 3 arr)
+;; (defn repeat-n [numtimes arr outer-item]
+;;    (dotimes [inner-item numtimes] 
+;;      (let [ inner-vector (nth arr outer-item)] 
+;;        (cond (= outer-item inner-item) 
+;;           (nth inner-vector inner-item) :else 0) 
+;;        inner-vector)
+;;      )
+;; )
+
+;; (repeat-n 3 [10 [10 50 30] 30] 1)
+
+;; (defn outer-mat [numtimes arra ]
+;;   (dotimes [item numtimes]
+;;     (repeat-n numtimes arra item)
+;;     )
+;;   )
+
+;; (outer-mat 3 arr )
+
+;; (defn sh-current [n arr]
+;;   (dotimes [i n]
+;;     (println (nth arr i))))
+
+
+;; (sh-current 3 arr)
 
